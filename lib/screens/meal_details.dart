c3639f16a5e7dbe76ceca304bdee60b8b293e57f
Ana Cvasniuc <annycvasniuc@yahoo.com>
2024-10-30 08:15:57 +0100

    add more info to details screen

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -9,14 +9,60 @@ class MealDetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(meal.title),
+      appBar: AppBar(
+        title: Text(meal.title),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            //image
+            Image.network(
+              (meal.imageUrl),
+              height: 300,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+            //Ingredients
+            const SizedBox(height: 14),
+            Text(
+              'Ingredients',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 14),
+            for (final ingridient in meal.ingredients)
+              Text(
+                ingridient,
+                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                      color: Theme.of(context).colorScheme.onBackground,
+                    ),
+              ),
+            const SizedBox(height: 24),
+            Text(
+              'Steps',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 14),
+            for (final steps in meal.steps)
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 12,
+                  vertical: 8,
+                ),
+                child: Text(
+                  steps,
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                        color: Theme.of(context).colorScheme.onBackground,
+                      ),
+                ),
+              ),
+          ],
         ),
-        body: Image.network(
-          (meal.imageUrl),
-          height: 300,
-          width: double.infinity,
-          fit: BoxFit.cover,
-        ));
+      ),
+    );
   }
 }
