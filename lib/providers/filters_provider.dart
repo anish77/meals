021011c8301974ab98230c02a8bb0ad1f04943ec
Ana Cvasniuc <annycvasniuc@yahoo.com>
2024-11-28 08:09:03 +0100

    update icon for favorite meals

--- lib/providers/filters_provider.dart
+++ lib/providers/filters_provider.dart
@@ -1,4 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:meals/data/dummy_data.dart';
+import 'package:meals/providers/meals_provider.dart';
 
 enum Filter {
   glutenFree,
@@ -35,3 +37,25 @@ final filterProvider =
     StateNotifierProvider<FiltersNotifier, Map<Filter, bool>>(
   (ref) => FiltersNotifier(),
 );
+
+final filteredMealsProvider = Provider((ref) {
+  final meals = ref.watch(mealsProvider);
+  final activeFilters = ref.watch(filterProvider);
+  return meals.where(
+    (meal) {
+      if (activeFilters[Filter.glutenFree]! && !meal.isGlutenFree) {
+        return false;
+      }
+      if (activeFilters[Filter.lactoseFree]! && !meal.isLactoseFree) {
+        return false;
+      }
+      if (activeFilters[Filter.vegetarianFree]! && !meal.isVegetarian) {
+        return false;
+      }
+      if (activeFilters[Filter.veganFree]! && !meal.isVegan) {
+        return false;
+      }
+      return true;
+    },
+  ).toList();
+});
